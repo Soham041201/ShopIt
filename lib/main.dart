ec048d02594ee5381e562918402fcb6566b738d9
Soham041201 <sattigeri.soham@gmail.com>
2021-05-31 15:18:59 +0530

    Added some comments for reference & added the home page for making the changes

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'home/home.dart';
+
 void main() {
   runApp(MyApp());
 }
@@ -8,10 +10,16 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        home: Scaffold(
-      appBar: AppBar(
-        title: Text('Shop It'),backgroundColor: Colors.black,
+      theme: ThemeData(
+        brightness: Brightness.dark,
+        primaryColor: Colors.black,
+        fontFamily: 'SourceSansPro' // Use any font you want to use just add the same here to make it default. 
       ),
-    ));
+        home: Home() );
   }
 }
+//How to add the fonts
+//download the font family file form google fonts
+//extract the file
+//copy the contents into fonts file in the project
+//head over to pubspec.yaml file  
