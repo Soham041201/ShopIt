6afb8665dfef872a5c7f325df95b7407aa47a807
Soham041201 <sattigeri.soham@gmail.com>
2021-06-05 11:44:08 +0530

    comments added

---
+++ lib/Seller-Chinmayee/SellerHome.dart
@@ -0,0 +1,3 @@
+// Create a home page for the user where he will be able to add the list of items which will be available to the seller
+
+// Hint: Add a floating button and in the property of onpressed find a method to add the images.abstract
--- lib/User-Saurav/UserHome.dart
+++ lib/User-Saurav/UserHome.dart
@@ -1,3 +1,3 @@
 // Create a home page for the user where he will be able to see the list of items added by seller.
 
-// Hint: Use List view widget to show the list
+// Hint: Use List view widget to show the list.
