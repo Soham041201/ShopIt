43f145d53a4eb9939da082fe9aad419766b7dfb2
Soham041201 <sattigeri.soham@gmail.com>
2021-06-05 11:43:13 +0530

    assigned task

--- lib/home/home.dart
+++ lib/home/home.dart
@@ -1,13 +1,12 @@
 import 'package:flutter/material.dart';
 
-
-
 class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('ShopIt'),),
+        title: Text('ShopIt'),
+      ),
     );
   }
 }
